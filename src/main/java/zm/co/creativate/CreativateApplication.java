e5e42a757475bb7873cabe4bff2b682491e537ee
Mumbi Chishimba <chishimba.mumbi@gmail.com>
2017-07-30 15:04:24 +0200

    Java EE writers application
    This is a simple Maven Based Java EE application for writers to post and
    talk about their work.

---
+++ src/main/java/zm/co/creativate/CreativateApplication.java
@@ -0,0 +1,12 @@
+package zm.co.creativate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativateApplication.class, args);
+	}
+}
